2f63404fc4b731941e6e576d7f96e2ab3545afc6
wmasarczyk <wojciech.masarczyk@gmail.com>
2022-11-08 10:26:41 +0000

    Black formatting and resolving flake8 errors

--- src/make_reconstructions.py
+++ src/make_reconstructions.py
@@ -8,22 +8,43 @@ import torch
 def make_reconstructions_from_batch(batch, save_dir, epoch, tokenizer):
     check_batch(batch)
 
-    original_frames = tensor_to_np_frames(rearrange(batch['observations'], 'b t c h w -> b t h w c'))
+    original_frames = tensor_to_np_frames(
+        rearrange(batch["observations"], "b t c h w -> b t h w c")
+    )
     all = [original_frames]
 
     rec_frames = generate_reconstructions_with_tokenizer(batch, tokenizer)
     all.append(rec_frames)
 
-    for i, image in enumerate(map(Image.fromarray, np.concatenate(list(np.concatenate((original_frames, rec_frames), axis=-2)), axis=-3))):
-        image.save(save_dir / f'epoch_{epoch:03d}_t_{i:03d}.png')
+    for i, image in enumerate(
+        map(
+            Image.fromarray,
+            np.concatenate(
+                list(np.concatenate((original_frames, rec_frames), axis=-2)), axis=-3
+            ),
+        )
+    ):
+        image.save(save_dir / f"epoch_{epoch:03d}_t_{i:03d}.png")
 
     return
 
 
 def check_batch(batch):
-    assert sorted(batch.keys()) == ['actions', 'ends', 'mask_padding', 'observations', 'rewards']
-    b, t, _, _, _ = batch['observations'].shape  # (B, T, C, H, W)
-    assert batch['actions'].shape == batch['rewards'].shape == batch['ends'].shape == batch['mask_padding'].shape == (b, t)
+    assert sorted(batch.keys()) == [
+        "actions",
+        "ends",
+        "mask_padding",
+        "observations",
+        "rewards",
+    ]
+    b, t, _, _, _ = batch["observations"].shape  # (B, T, C, H, W)
+    assert (
+        batch["actions"].shape
+        == batch["rewards"].shape
+        == batch["ends"].shape
+        == batch["mask_padding"].shape
+        == (b, t)
+    )
 
 
 def tensor_to_np_frames(inputs):
@@ -38,10 +59,10 @@ def check_float_btw_0_1(inputs):
 @torch.no_grad()
 def generate_reconstructions_with_tokenizer(batch, tokenizer):
     check_batch(batch)
-    inputs = rearrange(batch['observations'], 'b t c h w -> (b t) c h w')
+    inputs = rearrange(batch["observations"], "b t c h w -> (b t) c h w")
     outputs = reconstruct_through_tokenizer(inputs, tokenizer)
-    b, t, _, _, _ = batch['observations'].size()
-    outputs = rearrange(outputs, '(b t) c h w -> b t h w c', b=b, t=t)
+    b, t, _, _, _ = batch["observations"].size()
+    outputs = rearrange(outputs, "(b t) c h w -> b t h w c", b=b, t=t)
     rec_frames = tensor_to_np_frames(outputs)
     return rec_frames
 
@@ -49,5 +70,7 @@ def generate_reconstructions_with_tokenizer(batch, tokenizer):
 @torch.no_grad()
 def reconstruct_through_tokenizer(inputs, tokenizer):
     check_float_btw_0_1(inputs)
-    reconstructions = tokenizer.encode_decode(inputs, should_preprocess=True, should_postprocess=True)
+    reconstructions = tokenizer.encode_decode(
+        inputs, should_preprocess=True, should_postprocess=True
+    )
     return torch.clamp(reconstructions, 0, 1)
--- src/models/tokenizer/tokenizer.py
+++ src/models/tokenizer/tokenizer.py
@@ -23,7 +23,14 @@ class TokenizerEncoderOutput:
 
 
 class Tokenizer(nn.Module):
-    def __init__(self, vocab_size: int, embed_dim: int, encoder: Encoder, decoder: Decoder, with_lpips: bool = True) -> None:
+    def __init__(
+        self,
+        vocab_size: int,
+        embed_dim: int,
+        encoder: Encoder,
+        decoder: Decoder,
+        with_lpips: bool = True,
+    ) -> None:
         super().__init__()
         self.vocab_size = vocab_size
         self.encoder = encoder
@@ -37,7 +44,12 @@ class Tokenizer(nn.Module):
     def __repr__(self) -> str:
         return "tokenizer"
 
-    def forward(self, x: torch.Tensor, should_preprocess: bool = False, should_postprocess: bool = False) -> Tuple[torch.Tensor]:
+    def forward(
+        self,
+        x: torch.Tensor,
+        should_preprocess: bool = False,
+        should_postprocess: bool = False,
+    ) -> Tuple[torch.Tensor]:
         outputs = self.encode(x, should_preprocess)
         decoder_input = outputs.z + (outputs.z_quantized - outputs.z).detach()
         reconstructions = self.decode(decoder_input, should_postprocess)
@@ -45,21 +57,33 @@ class Tokenizer(nn.Module):
 
     def compute_loss(self, batch: Batch, **kwargs: Any) -> LossWithIntermediateLosses:
         assert self.lpips is not None
-        observations = self.preprocess_input(rearrange(batch['observations'], 'b t c h w -> (b t) c h w'))
-        z, z_quantized, reconstructions = self(observations, should_preprocess=False, should_postprocess=False)
+        observations = self.preprocess_input(
+            rearrange(batch["observations"], "b t c h w -> (b t) c h w")
+        )
+        z, z_quantized, reconstructions = self(
+            observations, should_preprocess=False, should_postprocess=False
+        )
 
         # Codebook loss. Notes:
         # - beta position is different from taming and identical to original VQVAE paper
         # - VQVAE uses 0.25 by default
         beta = 1.0
-        commitment_loss = (z.detach() - z_quantized).pow(2).mean() + beta * (z - z_quantized.detach()).pow(2).mean()
+        commitment_loss = (z.detach() - z_quantized).pow(2).mean() + beta * (
+            z - z_quantized.detach()
+        ).pow(2).mean()
 
         reconstruction_loss = torch.abs(observations - reconstructions).mean()
         perceptual_loss = torch.mean(self.lpips(observations, reconstructions))
 
-        return LossWithIntermediateLosses(commitment_loss=commitment_loss, reconstruction_loss=reconstruction_loss, perceptual_loss=perceptual_loss)
+        return LossWithIntermediateLosses(
+            commitment_loss=commitment_loss,
+            reconstruction_loss=reconstruction_loss,
+            perceptual_loss=perceptual_loss,
+        )
 
-    def encode(self, x: torch.Tensor, should_preprocess: bool = False) -> TokenizerEncoderOutput:
+    def encode(
+        self, x: torch.Tensor, should_preprocess: bool = False
+    ) -> TokenizerEncoderOutput:
         if should_preprocess:
             x = self.preprocess_input(x)
         shape = x.shape  # (..., C, H, W)
@@ -67,11 +91,17 @@ class Tokenizer(nn.Module):
         z = self.encoder(x)
         z = self.pre_quant_conv(z)
         b, e, h, w = z.shape
-        z_flattened = rearrange(z, 'b e h w -> (b h w) e')
-        dist_to_embeddings = torch.sum(z_flattened ** 2, dim=1, keepdim=True) + torch.sum(self.embedding.weight**2, dim=1) - 2 * torch.matmul(z_flattened, self.embedding.weight.t())
+        z_flattened = rearrange(z, "b e h w -> (b h w) e")
+        dist_to_embeddings = (
+            torch.sum(z_flattened**2, dim=1, keepdim=True)
+            + torch.sum(self.embedding.weight**2, dim=1)
+            - 2 * torch.matmul(z_flattened, self.embedding.weight.t())
+        )
 
         tokens = dist_to_embeddings.argmin(dim=-1)
-        z_q = rearrange(self.embedding(tokens), '(b h w) e -> b e h w', b=b, e=e, h=h, w=w).contiguous()
+        z_q = rearrange(
+            self.embedding(tokens), "(b h w) e -> b e h w", b=b, e=e, h=h, w=w
+        ).contiguous()
 
         # Reshape to original
         z = z.reshape(*shape[:-3], *z.shape[1:])
@@ -80,7 +110,9 @@ class Tokenizer(nn.Module):
 
         return TokenizerEncoderOutput(z, z_q, tokens)
 
-    def decode(self, z_q: torch.Tensor, should_postprocess: bool = False) -> torch.Tensor:
+    def decode(
+        self, z_q: torch.Tensor, should_postprocess: bool = False
+    ) -> torch.Tensor:
         shape = z_q.shape  # (..., E, h, w)
         z_q = z_q.view(-1, *shape[-3:])
         z_q = self.post_quant_conv(z_q)
@@ -91,7 +123,12 @@ class Tokenizer(nn.Module):
         return rec
 
     @torch.no_grad()
-    def encode_decode(self, x: torch.Tensor, should_preprocess: bool = False, should_postprocess: bool = False) -> torch.Tensor:
+    def encode_decode(
+        self,
+        x: torch.Tensor,
+        should_preprocess: bool = False,
+        should_postprocess: bool = False,
+    ) -> torch.Tensor:
         z_q = self.encode(x, should_preprocess).z_quantized
         return self.decode(z_q, should_postprocess)
 
